9fd74a06687e8dc77fd260fdf50b2b5efed0cfb2
Tony Jiang <white87332@gmail.com>
2016-07-04 20:58:44 +0800

    update package

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -1,6 +1,7 @@
 var path = require('path');
 var webpack = require('webpack');
 var autoprefixer = require('autoprefixer');
+var flexibility = require('postcss-flexibility');
 
 module.exports = {
     entry:
@@ -50,7 +51,8 @@ module.exports = {
         }]
     },
     postcss: [
-        autoprefixer
+        autoprefixer,
+        flexibility
     ],
     plugins: [
         new webpack.optimize.OccurenceOrderPlugin(),
