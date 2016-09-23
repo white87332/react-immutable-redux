3638fcd23bb6dc5dbc4096169b4074421809280c
Tony Jiang <white87332@gmail.com>
2016-09-23 16:35:02 +0800

    update webpack config, package

--- webpack.development.config.js
+++ webpack.development.config.js
@@ -42,7 +42,7 @@ module.exports = {
             loaders: [
                 "style",
                 "css",
-                "sass",
+                "sass?outputStyle=compressed",
                 "postcss"
             ]
         },
